ab1e6b3284d72e72743ef6fbabffc4b26cf582c0
EmersonY <emersonyuri32@gmail.com>
2017-01-08 00:22:58 -0500

    banner with jquery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
 $(document).ready(function(){
-	alert('hi!');
+
+  var banner = {
+    parent: $('#banner'),
+    numSlides: $('#banner').children('.slide').length,
+    position: 1
+  }
+
+  banner.parent.children('.slide').first().css({
+    'left': 0
+  });
+
+  var heightBanner = function(){
+    var altitude = banner.parent.children('.slide').outerHeight();
+
+    banner.parent.css({
+      'height': altitude + 'px'
+    });
+    console.log(altitude);
+  }
+
+  heightBanner();
+
+  $(window).resize(function() {
+    heightBanner();
+  });
+
+  // console.log(banner.numSlides);
 });
