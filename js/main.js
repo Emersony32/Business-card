3f84a335a2700cb7f8f907ce92d30ed2d3e56c26
EmersonY <emersonyuri32@gmail.com>
2017-01-08 10:37:31 -0500

    top slider

--- js/main.js
+++ js/main.js
@@ -25,5 +25,46 @@ $(document).ready(function(){
     heightBanner();
   });
 
+// ************* Banner ***********
+
+  // Button next
+  $('#banner-next').on('click', function(n){
+    n.preventDefault();
+
+    if(banner.position < banner.numSlides) {
+
+      banner.parent.children().not('.active').css({
+        'left': '100%'
+      });
+
+      $('#banner .active').removeClass('active').next().addClass('active').animate({
+        'left': '0'
+      });
+
+      $('#banner .active').prev().animate({
+        'left': '-100%'
+      });
+
+      banner.position += 1;
+    } else {
+
+      $('#banner .active').animate({
+        'left': '-100%'
+      });
+
+      banner.parent.children().not('.active').css({
+        'left': '100%'
+      });
+
+      $('#banner .active').removeClass('active');
+      banner.parent.children('.slide').first().addClass('active').animate({
+        'left': 0
+      });
+
+      banner.position = 1;
+    }
+
+  });
+
   // console.log(banner.numSlides);
 });
